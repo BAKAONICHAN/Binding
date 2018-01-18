6c4dd51cf7e4d9012e4472cc650c5eed90e4919f
Nurik <tanatov1989@mail.ru>
2018-01-18 15:55:46 +0600

    Добавьте файлы проекта.

---
+++ BindingLesson/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Train_ticket> tickets;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tickets = new ObservableCollection<Train_ticket>
+            {
+                new Train_ticket{
+                    Id=1,
+                 Number="13",
+                 Price=10200,
+                 From="Astana",
+                 To="Almaty",
+                 ArrivalTime=DateTime.Now,
+                 DepartureTime=DateTime.Now.AddDays(12)
+
+                },
+       
+            new Train_ticket {
+                Id = 3,
+                Number = "14",
+                Price = 11000,
+                From = "Astana",
+                To = "Almaty",
+                ArrivalTime = DateTime.Now,
+                DepartureTime = DateTime.Now.AddDays(12)
+            },
+                  new Train_ticket{
+                  Id=2,
+                 Number="17",
+                 Price=2000,
+                 From="Astana",
+                 To="Almaty",
+                 ArrivalTime=DateTime.Now,
+                 DepartureTime=DateTime.Now.AddDays(12)
+                  }
+            };
+            DataContiner.ItemsSource = tickets;
+        }
+        private void DataContiner_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ BindingLesson/Train_ticket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingLesson
+{
+    public class Train_ticket : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+       private int id;
+            public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("id");
+            }
+        }
+       
+
+        //public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int Price { get; set; }
+        public string Number { get; set; }
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
